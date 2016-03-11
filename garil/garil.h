62ce47c7adb77221b28fcd6f03bab3982953ea78
You-Sheng Yang <vicamo@gmail.com>
2016-03-12 01:24:22 +0800

    garil: install two dummy headers

---
+++ garil/garil.h
@@ -0,0 +1,24 @@
+/* libgaril - Android RIL client library
+ * Copyright (C) 2016 You-Sheng Yang
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define __GARIL_GARIL_H_INSIDE__
+
+#include <garil/verion.h>
+
+#undef __GARIL_GARIL_H_INSIDE__
